7b146a5ff559205e1faa52b26038b1653db8800a
eedo_y <doup2001@naver.com>
2025-10-05 17:27:38 +0900

    💡style : 댓글 DTO 스웨거 변수 적용

--- src/main/java/bootcamp/kakao/community/platform/posts/comment/application/dto/CommentRequest.java
+++ src/main/java/bootcamp/kakao/community/platform/posts/comment/application/dto/CommentRequest.java
@@ -1,8 +1,16 @@
 package bootcamp.kakao.community.platform.posts.comment.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "[요청][댓글] 댓글 등록 Request", description = "댓글 등록을 위한 DTO입니다.")
 public record CommentRequest(
+        @Schema(description = "부모 댓글 아이디", example = "null")
         Long parentId,
+
+        @Schema(description = "게시글 아이디", example = "1")
         Long postId,
+
+        @Schema(description = "댓글 내용", example = "이 글에 공감합니다!")
         String content
 ) {
 }
--- src/main/java/bootcamp/kakao/community/platform/posts/comment/application/dto/CommentResponse.java
+++ src/main/java/bootcamp/kakao/community/platform/posts/comment/application/dto/CommentResponse.java
@@ -2,18 +2,29 @@ package bootcamp.kakao.community.platform.posts.comment.application.dto;
 
 import bootcamp.kakao.community.platform.posts.comment.domain.entity.Comment;
 import bootcamp.kakao.community.platform.user.application.dto.UserResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 
 import java.util.List;
 
+@Schema(
+        name = "[응답][댓글] 댓글 응답 Response",
+        description = "댓글 정보를 응답하기 위한 DTO입니다."
+)
 @Builder
 public record CommentResponse(
-
+        @Schema(description = "댓글 작성자 정보")
         UserResponse user,
+
+        @Schema(description = "부모 댓글 아이디", example = "null")
         Long parentId,
+
+        @Schema(description = "댓글 본문", example = "정말 좋은 게시글이네요!")
         String content,
+
+        @Schema(description = "댓글 수정 가능 여부", example = "true")
         boolean editable
 ) {
 
