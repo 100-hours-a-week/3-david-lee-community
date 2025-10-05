2d6ee0dd64225e00ac540cc584df1e978ead9f05
eedo_y <doup2001@naver.com>
2025-10-05 17:52:44 +0900

    💡style : 로그인 스웨거 주석 추가

--- src/main/java/bootcamp/kakao/community/security/auth/application/dto/LoginRequest.java
+++ src/main/java/bootcamp/kakao/community/security/auth/application/dto/LoginRequest.java
@@ -1,7 +1,13 @@
 package bootcamp.kakao.community.security.auth.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "[요청][인증] 로그인 Request", description = "로그인 요청을 위한 DTO입니다.")
 public record LoginRequest(
+        @Schema(description = "사용자 이메일", example = "ktbcloud@kakao.com")
         String email,
+
+        @Schema(description = "사용자 비밀번호", example = "Hello123!World")
         String password
 ) {
 }
