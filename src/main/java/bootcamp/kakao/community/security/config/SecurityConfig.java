9409ed06f251f8b82a5fdd71c11bdc840a7af1a7
eedo_y <doup2001@naver.com>
2025-10-22 15:40:24 +0900

    ✨feat : 시큐리티 인증/인가 부분 추가하기

--- src/main/java/bootcamp/kakao/community/security/config/SecurityConfig.java
+++ src/main/java/bootcamp/kakao/community/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package bootcamp.kakao.community.security.config;
 import bootcamp.kakao.community.security.jwt.filter.JwtDeniedHandler;
 import bootcamp.kakao.community.security.jwt.filter.JwtFailureHandler;
 import bootcamp.kakao.community.security.jwt.filter.JwtFilter;
+import bootcamp.kakao.community.security.jwt.filter.RequestMatcherHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import static bootcamp.kakao.community.platform.user.domain.entity.UserRole.ADMIN;
+import static bootcamp.kakao.community.platform.user.domain.entity.UserRole.MEMBER;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -21,7 +25,9 @@ public class SecurityConfig {
     private final JwtFailureHandler jwtFailureHandler;
     private final JwtDeniedHandler jwtDeniedHandler;
 
+    private final RequestMatcherHolder requestMatcherHolder;
     private final CorsConfigurationSource corsConfigurationSource;
+
     /**
      * SecurityFilterChain 설정
      */
@@ -34,8 +40,15 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(AbstractHttpConfigurer::disable)
-                /// 인가
-                .authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(requestMatcherHolder.getRequestMatchersByMinRole(null))
+                        .permitAll()
+                        .requestMatchers(requestMatcherHolder.getRequestMatchersByMinRole(MEMBER))
+                        .hasAnyAuthority(ADMIN.getRole(), MEMBER.getRole())
+                        .requestMatchers(requestMatcherHolder.getRequestMatchersByMinRole(ADMIN))
+                        .hasAnyAuthority(ADMIN.getRole())
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) /// UsernamePasswordAuthenticationFilter.class 전에 실행하도록
                 .exceptionHandling(exception -> {
                     exception.authenticationEntryPoint(jwtFailureHandler)
@@ -54,3 +67,4 @@ public class SecurityConfig {
 
 
 }
+
