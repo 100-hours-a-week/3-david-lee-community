2ae9e28d0b45074089e68eb3a14e6ca0236a5059
eedo_y <doup2001@naver.com>
2025-10-22 14:45:48 +0900

    ♻️refactor : JWT 예외처리는 핸들러에서 처리하기에 삭제

--- src/main/java/bootcamp/kakao/community/common/exception/GlobalExceptionHandler.java
+++ src/main/java/bootcamp/kakao/community/common/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import bootcamp.kakao.community.common.response.CustomException;
 import bootcamp.kakao.community.common.response.ErrorCode;
 import bootcamp.kakao.community.common.response.FieldErrorResponse;
 import bootcamp.kakao.community.common.response.code.CommonErrorCode;
-import bootcamp.kakao.community.security.jwt.filter.JwtAuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.http.HttpStatus;
@@ -34,23 +33,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(ApiResponse.fail(e));
-    }
-
-    /// JWT 관련 커스템 에러 처리
-    @ExceptionHandler(JwtAuthenticationException.class)
-    public ResponseEntity<ApiResponse<?>> handleAuthenticationException(JwtAuthenticationException e) {
-
-        /// 에러 코드
-        ErrorCode errorCode = e.getErrorCode();
-
-        /// 기본 에러 코드로 응답 생성
-        CustomException exception = new CustomException(errorCode);
-        var response = ApiResponse.fail(exception);
-
-        /// 응답
-        return ResponseEntity
-                .status(errorCode.getHttpStatus())
-                .body(response);
     }
 
     /// @Valid 파라미터 에러 처리
