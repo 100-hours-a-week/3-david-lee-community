be4ede59fc3d4aed3ea7cb1584eba0a24432dd3b
eedo_y <doup2001@naver.com>
2025-10-25 17:15:23 +0900

    ✨feat : 댓글 작성 시 DTO 응답 제공

--- src/main/java/bootcamp/kakao/community/common/exception/GlobalExceptionHandler.java
+++ src/main/java/bootcamp/kakao/community/common/exception/GlobalExceptionHandler.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
@@ -170,8 +171,8 @@ public class GlobalExceptionHandler {
 
     /// 타입 오류
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(UnexpectedTypeException.class)
-    public ApiResponse<?> handleJUnexpectedTypeException(UnexpectedTypeException e) {
+    @ExceptionHandler({UnexpectedTypeException.class, MethodArgumentTypeMismatchException.class})
+    public ApiResponse<?> handleJUnexpectedTypeException(Exception e) {
 
         /// 에러 이유 로그 찍기
         log.error(e.getMessage());
