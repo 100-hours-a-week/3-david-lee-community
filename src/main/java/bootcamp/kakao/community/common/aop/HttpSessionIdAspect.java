6e782bb18e540e629cf6bf8dad58c2727fa4a5a3
eedo_y <doup2001@naver.com>
2025-10-28 17:48:21 +0900

    ✨ feat : AOP를 통한 userId 추출하기

--- src/main/java/bootcamp/kakao/community/common/aop/HttpSessionIdAspect.java
+++ src/main/java/bootcamp/kakao/community/common/aop/HttpSessionIdAspect.java
@@ -2,12 +2,19 @@ package bootcamp.kakao.community.common.aop;
 
 import bootcamp.kakao.community.common.response.CustomException;
 import bootcamp.kakao.community.common.response.code.CommonErrorCode;
+import bootcamp.kakao.community.common.util.HttpUtil;
 import bootcamp.kakao.community.security.session.application.SessionProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Optional;
 
 @Component
 @Aspect
@@ -15,21 +22,31 @@ import org.springframework.stereotype.Component;
 public class HttpSessionIdAspect {
 
     private final SessionProvider sessionProvider;
+    private final HttpServletRequest request;
+    private final HttpUtil httpUtil;
 
-    /// HttpSession 가져오는 AOP
-    @Around("@annotation(bootcamp.kakao.community.common.aop.HttpSessionId)")
-    public Object getUserByCookie(ProceedingJoinPoint pjp) throws Throwable {
+    @Around("execution(* *(.., @bootcamp.kakao.community.common.aop.HttpSessionId (*), ..))")
+    public Object injectUserId(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        Object[] args = joinPoint.getArgs();
+        Annotation[][] paramAnnotations = method.getParameterAnnotations();
 
-        /// 해당 세션 ID를 가진 유저가 실제로 존재 하는지 체크
-        Long userId = sessionProvider.getUserBySession(pjp.getArgs()[0].toString());
-
-        /// 없으면 예외처리
-        if (userId == null) {
-
-            /// 401 매핑
-            throw new CustomException(CommonErrorCode.UNAUTHORIZED);
+        for (int i = 0; i < paramAnnotations.length; i++) {
+            for (Annotation annotation : paramAnnotations[i]) {
+                if (annotation instanceof HttpSessionId) {
+                    Optional<String> sessionId = httpUtil.getSessionId(request);
+                    Long userId = sessionProvider.getUserBySession(sessionId);
+                    if (userId == null) {
+                        throw new CustomException(CommonErrorCode.UNAUTHORIZED);
+                    }
+                    args[i] = userId; // 파라미터에 유저 ID 주입
+                }
+            }
         }
-        return pjp.proceed();}
+
+        return joinPoint.proceed(args);
+    }
 
 }
 
