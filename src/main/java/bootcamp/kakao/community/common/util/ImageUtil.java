fd4ce6d8cc826bd0e2c35f2ea2067509750fc5f2
eedo_y <doup2001@naver.com>
2025-11-02 13:34:28 +0900

    🔥fix : 이미지 URL 없음에도, URL 출력되는 문제 수정

--- src/main/java/bootcamp/kakao/community/common/util/ImageUtil.java
+++ src/main/java/bootcamp/kakao/community/common/util/ImageUtil.java
@@ -21,13 +21,21 @@ public class ImageUtil {
         ImageUtil.region = region;
     }
 
+    /// Key 생성하는 함수
     public static String generateKey(String fileName) {
         String key = UUID.randomUUID().toString();
         String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
         return key + "." + extension;
     }
 
+    /// key를 URL로 만드는 함수
     public static String getUrlByKey(String key) {
+
+        /// null이면 없이 응답
+        if (key == null) {
+            return null;
+        }
+
         return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key;
     }
 }
