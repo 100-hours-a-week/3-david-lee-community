6c6ea9fb2f52fe52789cffe96b54693f0072a79d
eedo_y <doup2001@naver.com>
2025-11-28 11:48:34 +0900

    💡 style : 이용약관 사이트 수정

--- src/main/java/bootcamp/kakao/community/platform/terms/presentation/PolicyController.java
+++ src/main/java/bootcamp/kakao/community/platform/terms/presentation/PolicyController.java
@@ -1,5 +1,6 @@
 package bootcamp.kakao.community.platform.terms.presentation;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/v1/policy")
 public class PolicyController {
 
+    @Value("${cors.front.host}")
+    private String frontendUrl;
+
     /// 약관에 대한 내용
     @GetMapping("/terms")
     public String terms(Model model) {
-        model.addAttribute("companyName", "아무말 대 잔치");
+        model.addAttribute("companyName", "개발자커뮤니티");
+        model.addAttribute("frontendUrl", frontendUrl);
         return "policy/terms";
     }
 
     /// 개인정보에 대한 내용
     @GetMapping("/privacy")
     public String privacy(Model model) {
-        model.addAttribute("companyName", "아무말 대 잔치");
+        model.addAttribute("companyName", "개발자커뮤니티");
+        model.addAttribute("frontendUrl", frontendUrl);
         return "policy/privacy";
     }
 }
