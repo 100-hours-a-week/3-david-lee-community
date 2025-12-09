05f3098f1822a2c822b8ddf7605a4cc42055d84b
eedo_y <doup2001@naver.com>
2025-12-09 16:19:23 +0900

    ♻️refactor : 게시글 이미지 개수 검증 조건 추가

--- src/main/java/bootcamp/kakao/community/platform/posts/post/application/dto/PostUpdateRequest.java
+++ src/main/java/bootcamp/kakao/community/platform/posts/post/application/dto/PostUpdateRequest.java
@@ -1,6 +1,7 @@
 package bootcamp.kakao.community.platform.posts.post.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public record PostUpdateRequest(
         String content,
 
         @Schema(description = "첨부 이미지 URL 리스트", example = "[\"image1.png\", \"image2.png\"]")
+        @Size(max = 10,message = "이미지는 최대 10장까지 가능합니다.")
         List<String> imageKeys
 ) {
 }
